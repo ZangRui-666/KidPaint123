20fe627841bf485776423393f1f65a07c4dd7bc0
👉Your name👈 <Your email>
2021-11-16 12:07:30 +0800

    add LoginUI

--- src/KidPaint.java
+++ src/KidPaint.java
@@ -1,8 +1,9 @@
 
 public class KidPaint {
-	String name;
+	static String name;
 	public static void main(String[] args) {
-
+		LoginUI loginUI = new LoginUI();
+		name = loginUI.getName();
 		UI ui = UI.getInstance();			// get the instance of UI
 		ui.setData(new int[50][50], 20);	// set the data array and block size. comment this statement to use the default data array and block size.
 		ui.setVisible(true);				// set the ui
--- src/LoginUI.java
+++ src/LoginUI.java
@@ -1,79 +1,92 @@
-    import javax.swing.*;
+import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.*;
-    public class LoginUI extends JFrame implements ActionListener{
-        JLabel jl1,jl2,jl3;
-        JTextField jtf;
-        JPasswordField jpf;
-        JButton jb1,jb2,jb3;
-        JPanel jp1,jp2,jp3,jp4;
 
-        public LoginUI(){
-            Container container = this.getContentPane();
+public class LoginUI extends JFrame implements ActionListener {
+    JLabel jl1, jl2, jl3;
+    JTextField jtf;
+    JPasswordField jpf;
+    JButton jb1, jb2, jb3;
+    JPanel jp1, jp2, jp3, jp4;
+    String name;
+    boolean isLogin = false;
 
-            Font font1 = new Font("SansSerif", Font.BOLD, 20);
-            JTextField textField1 = new JTextField();
-            textField1.setFont(font1);
-            textField1.setText("KidPaint");
+    public LoginUI() {
+        Container container = this.getContentPane();
 
-
-            Font font2 = new Font("Georgia", Font.PLAIN, 15);
-            JTextField textField2 = new JTextField();
-            textField2.setFont(font1);
-            textField2.setText("This application allows  users to use the shared drawing board to draw at the " +
-                    "same time and synchronize it to other users in the same group. After entering your name, click " +
-                    "the login button to select an existing group or create a new group, and then start enjoying the " +
-                    "fun of drawing!");
-
-            //container.add(textField2,BorderLayout.NORTH);
-
-            jl1=new JLabel("Enter your name：");
-            jl2=new JLabel("Enter your licence number(For normal user, it's 123456");
-
-            jp1=new JPanel();
-            jp1.add(jl1);
-            jp1.add(jtf);
-
-            jp2=new JPanel();
-            jp2.add(jl2);
-            jpf=new JPasswordField(16);
-            jp2.add(jpf);
-
-            jb1=new JButton("Submit");
+        Font font1 = new Font("SansSerif", Font.BOLD, 20);
+        JTextField textField1 = new JTextField();
+        textField1.setFont(font1);
+        textField1.setText("KidPaint");
 
 
-            this.add(jp1);
-            this.add(jp2);
-            this.add(jb1);
+        Font font2 = new Font("Georgia", Font.PLAIN, 15);
+        JTextField textField2 = new JTextField();
+        textField2.setFont(font1);
+        textField2.setText("This application allows  users to use the shared drawing board to draw at the " +
+                "same time and synchronize it to other users in the same group. After entering your name, click " +
+                "the login button to select an existing group or create a new group, and then start enjoying the " +
+                "fun of drawing!");
 
-            jb1.addActionListener(this);
-            this.setSize(400,300);
-            this.setLocation(300,300);
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setVisible(true);
-        }
-        public static void main(String[] args) {
-            new LoginUI();
-        }
-        @Override
-        public void actionPerformed(ActionEvent e) {
+        //container.add(textField2,BorderLayout.NORTH);
 
-            if(e.getSource()==jb1) {
-                String student="123456";
-                String admin = "123321";
+        jl1 = new JLabel("Enter your name：");
+        jl2 = new JLabel("Enter your licence number(For normal user, it's 123456");
 
-                String j1=jtf.getText();
-                String p1=String.copyValueOf(jpf.getPassword());
+        jp1 = new JPanel();
+        jtf = new JTextField();
+        jp1.add(jl1);
+        jp1.add(jtf);
+
+        jp2 = new JPanel();
+        jp2.add(jl2);
+        jpf = new JPasswordField(16);
+        jp2.add(jpf);
+
+        jb1 = new JButton("Submit");
 
 
+        this.add(jp1);
+        this.add(jp2);
+        this.add(jb1);
 
+        jb1.addActionListener(this);
+        this.setSize(400, 300);
+        this.setLocation(300, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LoginUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == jb1) {
+            String student = "123456";
+            String admin = "123321";
+
+            String name = jtf.getText();
+            String p1 = String.copyValueOf(jpf.getPassword());
+
+            if (p1.equals(student)) {
+                this.name = name;
+                this.isLogin = true;
             }
-
         }
     }
-    class test extends JPanel{
 
+    public String getName() {
+        return name;
     }
+
+}
+
+class test extends JPanel {
+
+}
 
 
