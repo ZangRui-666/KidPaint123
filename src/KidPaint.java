d533df5e8a4b0caac8eb00402a94d90d92a2a111
👉Your name👈 <Your email>
2021-11-19 12:41:30 +0800

    Almost done

--- src/KidPaint.java
+++ src/KidPaint.java
@@ -22,7 +22,7 @@ public class KidPaint {
 
 		GroupUI groupUI = GroupUI.getInstance();
 		groupUI.setVisible(true);
-		while (studioName==""){}
+		while (studioName.equals("")){}
 		groupUI.setVisible(false);
 
 		UI ui = UI.getInstance();			// get the instance of UI
