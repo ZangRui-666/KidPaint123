2626983cdd70a0c2d1bdbc67c973dfd3cf5ae08c
Adela0717 <19251491@life.hkbu.edu.hk>
2021-11-22 14:45:06 +0800

    aoligei

--- src/UI.java
+++ src/UI.java
@@ -486,7 +486,7 @@ public class UI extends JFrame {
                     int[][] newData = new int[20][20];
                     for (int i = 0; i < 20; i++)
                         for (int j = 0; j < 20; j++)
-                            newData[i][j] = in.readInt();
+                            newData[j][i] = in.readInt();
                     updatePainting(newData);
                     if (KidPaint.isServer) {
                         String name = clientsNames.get(connectedClients.indexOf(socket));
